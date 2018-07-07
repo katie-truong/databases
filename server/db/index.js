a387bda0ce1b50b5340cc54d4062a663eab3b077
katie-truong <katietruong.ucdavis@gmail.com>
2018-07-06 19:56:13 -0700

    Implement database

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,16 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+exports.connection = mysql.createConnection({
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
+
+//connection.connect();
+
+//connection.end();
+
+//in model
+db.connection.connect()
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,40 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  userID int,
+  userName varchar(20),
+  PRIMARY KEY (userID)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageID int,
+  message text,
+  userID int,
+  PRIMARY KEY (messageID),
+  FOREIGN KEY (userID) REFERENCES users(userID)
 );
+
+CREATE TABLE rooms (
+  roomID int,
+  roomName varchar(20),
+  PRIMARY KEY (roomID)
+);
+
+CREATE TABLE user_room (
+  userID int,
+  roomID int,
+  FOREIGN KEY (userID) REFERENCES users(userID),
+  FOREIGN KEY (roomID) REFERENCES rooms(roomID)
+);
+
+CREATE TABLE friends (
+  user1ID int,
+  user2ID int,
+  FOREIGN KEY (user1ID) REFERENCES users(userID),
+  FOREIGN KEY (user2ID) REFERENCES users(userID)
+)
 
 /* Create other tables and define schemas for them here! */
 
